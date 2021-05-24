5e826577e201c14e676c25e7827f1cf47689b1c3
Vinicius Mussato <vinicius.mussato@gmail.com>
2021-05-23 23:20:54 -0300

    info component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { InfoComponent } from './home/info/info.component';
 import { TopBarComponent } from './home/top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    TopBarComponent
+    TopBarComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
