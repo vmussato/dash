418c2b2d2d69beae95b83d8dfe8e532e0429a291
Vinicius Mussato <vinicius.mussato@gmail.com>
2021-05-23 23:59:34 -0300

    spliting components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { InfoComponent } from './home/info/info.component';
 import { TopBarComponent } from './home/top-bar/top-bar.component';
+import { NewTransactionComponent } from './home/info/new-transaction/new-transaction.component';
+import { ExtractComponent } from './home/info/extract/extract.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     TopBarComponent,
-    InfoComponent
+    InfoComponent,
+    NewTransactionComponent,
+    ExtractComponent
   ],
   imports: [
     BrowserModule,
